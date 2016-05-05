b5fb5678dab71ece1e6603cdb8e084656fb2f813
PhillJaySaw <philljaysaw@gmail.com>
2016-05-05 09:45:03 +0200

    Adjusted the offset of scrollTo, so that the fixed nav doesen't cover the content.

--- scripts/navscroll.js
+++ scripts/navscroll.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
   $('ul li a').click(function(evn){
     evn.preventDefault();
-    $(window).scrollTo(this.hash, this.hash);
+    $(window).scrollTo(this.hash, this.hash, {
+      offset: -80
+    });
   });
 
 });
